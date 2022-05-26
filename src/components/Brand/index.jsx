cfd0a2f40217831616d5998d308c88371104758c
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2022-05-26 19:34:40 +0700

    + implement brand component

--- src/components/Brand/index.jsx
+++ src/components/Brand/index.jsx
@@ -1,9 +1,25 @@
-import './brand.css'
+import React from 'react';
+import './brand.css';
+import { atlassian, dropbox, google, shopify, slack } from './imports';
 
-const Brand = () => {
-  return (
-    <div>Brand</div>
-  )
-}
+const Brand = () => (
+	<div className="gpt3__brand section__padding">
+		<div>
+			<img src={google} alt="Google" />
+		</div>
+		<div>
+			<img src={slack} alt="Slack" />
+		</div>
+		<div>
+			<img src={atlassian} alt="Atlassian" />
+		</div>
+		<div>
+			<img src={dropbox} alt="Dropbox" />
+		</div>
+		<div>
+			<img src={shopify} alt="Shopify" />
+		</div>
+	</div>
+);
 
-export default Brand
+export default Brand;
