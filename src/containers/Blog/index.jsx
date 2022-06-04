0c751e9dc56c247600e88f904e0b4ac3021d15c1
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2022-06-04 11:23:59 +0700

    + implement blog component

--- src/containers/Blog/index.jsx
+++ src/containers/Blog/index.jsx
@@ -1,9 +1,46 @@
-import './blog.css'
+import { Article } from '../../components';
+import './blog.css';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 
-const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
-}
+const Blog = () => (
+	<div className="gpt3__blog section__padding" id="blog">
+		<div className="gpt3__blog-heading">
+			<h1 className="gradient__text">
+				A lot is happening, <br /> We are blogging about it.
+			</h1>
+		</div>
+		<div className="gpt3__blog-container">
+			<div className="gpt3__blog-container_groupA">
+				<Article
+					imgUrl={blog01}
+					date="Feb 22, 2022"
+					text="GPT-3 and Open  AI is the future. Let us explore how it is?"
+				/>
+			</div>
+			<div className="gpt3__blog-container_groupB">
+				<Article
+					imgUrl={blog02}
+					date="Feb 22, 2022"
+					text="GPT-3 and Open  AI is the future. Let us explore how it is?"
+				/>
+				<Article
+					imgUrl={blog03}
+					date="Feb 22, 2022"
+					text="GPT-3 and Open  AI is the future. Let us explore how it is?"
+				/>
+				<Article
+					imgUrl={blog04}
+					date="Feb 22, 2022"
+					text="GPT-3 and Open  AI is the future. Let us explore how it is?"
+				/>
+				<Article
+					imgUrl={blog05}
+					date="Feb 22, 2022"
+					text="GPT-3 and Open  AI is the future. Let us explore how it is?"
+				/>
+			</div>
+		</div>
+	</div>
+);
 
-export default Blog
+export default Blog;
